400e9d7af413ccab98fc5d282470a826d8328ae3
sylvain-d-guimond <69218960+sylvain-d-guimond@users.noreply.github.com>
2023-01-11 23:58:13 +0100

    Update states

---
+++ Runtime/Scripts/EffectBillboard.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+[ExecuteInEditMode]
+public class EffectBillboard : MonoBehaviour
+{
+    public Transform PointA;
+    public Transform PointB;
+
+    public void SetPointA(Transform t)
+    {
+        PointA = t;
+    }
+
+    public void SetPointB(Transform t)
+    {
+        PointB = t;
+    }
+
+    void FixedUpdate()
+    {
+        if (PointA != null && PointB != null)
+        {
+            var fingerSpan = PointB.position - PointA.position;
+            var midpoint = PointA.position + (fingerSpan) / 2;
+            transform.position = midpoint;
+            var scale = fingerSpan.magnitude;
+            transform.localScale = new Vector3(scale, scale, scale);
+            var firstCross = Vector3.Cross(fingerSpan, Camera.main.transform.position - midpoint);
+            var secondCross = Vector3.Cross(fingerSpan, firstCross);
+            transform.rotation = Quaternion.LookRotation(firstCross, secondCross);
+        }
+    }
+}
--- Runtime/Scripts/State.cs
+++ Runtime/Scripts/State.cs
@@ -1,50 +1,101 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+[Serializable]
 public class State : MonoBehaviour
 {
     [SerializeField]
     private bool _active;
     [SerializeField]
-    private StateGroup _group;
+    private State _parent;
     [SerializeField]
     private bool _default;
+    [SerializeField]
+    private GroupTypes _type;
+    private bool _initialized;
+    private List<State> _subStates = new List<State>();
+
+    public UnityEvent OnActivate;
+    public UnityEvent OnDeactivate;
 
     public bool Active
     {
         get => _active;
         set
         {
-            if (_active != value)
+            if (_active != value || !_initialized)
             {
                 if (value)
                 {
                     OnActivate.Invoke();
-                    if (_group != null) 
-                        _group.Activate(this);
+                    if (_parent != null && _parent._type == GroupTypes.Single)
+                        _parent.DeactivateOthers(this);
+
+                    ActivateDefault();
                 }
                 else
                 {
                     OnDeactivate.Invoke();
                 }
 
-                _active = value;
+                gameObject.SetActive(_active = value);
+                _initialized = true;
             }
         }
     }
 
-    public UnityEvent OnActivate;
-    public UnityEvent OnDeactivate;
-
     private void Awake()
     {
-        if (_group != null) _group.States.Add(this);
+        if (_parent != null) _parent.AddChild(this);
     }
 
     private void Start()
     {
-        if (_default) { Active = true; }
+        if (_parent == null) ActivateDefault();
+    }
+
+    public void AddChild(State aState)
+    {
+        _subStates.Add(aState);
+    }
+
+    public void ActivateDefault()
+    {
+        foreach (var child in _subStates)
+        {
+            if (child._default) child.Active = true;
+        }
+    }
+
+    public void DeactivateOthers(State state)
+    {
+        if (_type == GroupTypes.Single)
+        {
+            foreach (var aState in _subStates)
+            {
+                if (aState != state)
+                {
+                    aState.Active = false;
+                }
+            }
+        }
+    }
+
+    public void Deactivate()
+    {
+        foreach (var aState in _subStates)
+        {
+            aState.Active = false;
+        }
     }
 }
+
+public enum GroupTypes
+{
+    Single,
+    Multiple
+}
+
